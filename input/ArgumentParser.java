42cb730b3f2cb0e57af39dfd5ccac50ca34d32e6
Sjoerd Schouten <sjoerdschouten@pm.me>
2025-06-06 13:54:38 +0200

    refactor ArgumentParser

--- input/ArgumentParser.java
+++ input/ArgumentParser.java
@@ -11,44 +11,66 @@ public class ArgumentParser {
     private String writePath = "";
     private String readPath = "";
 
-    // todo: I think we can refactor this and let the class return an object that holds the entire configuration.
-
     public void parseArgs(String[] args) {
         if (args.length == 0) {
             printUsage();
             throw new IllegalArgumentException("No arguments provided. Use -dataType <type> and -sortingType <type>.");
         }
+
         for (int i = 0; i < args.length; i++) {
-            if ("-dataType".equals(args[i]) && i + 1 < args.length) {
-                try {
-                    dataType = DataType.fromString(args[i + 1]);
-                } catch (IllegalArgumentException e) {
-                    throw new IllegalArgumentException("Invalid data type: " + args[i + 1], e);
-                }
-
-                i++; // skip next arg
-            } else if ("-sortingType".equals(args[i]) && i + 1 < args.length) {
-                try {
-                    sortingType = SortingType.fromString(args[i + 1]);
-                    // todo: numbers can't have sortingType natural.
-                } catch (IllegalArgumentException e) {
-                    throw new IllegalArgumentException("Invalid sorting type: " + args[i + 1], e);
-                }
-                i++; // skip next arg
-            } else if ("-inputFile".equals(args[i]) && i + 1 < args.length) {
-                readFromFile = true;
-                readPath = args[i + 1];
-                i++;
-            } else if ("-outputFile".equals(args[i]) && i + 1 < args.length) {
-                writeToFile = true;
-                writePath = args[i+1];
-                i++;
-
-            } else {
-                printUsage();
-                throw new IllegalArgumentException("Unknown argument: " + args[i]);
+            switch (args[i]) {
+                case "-dataType":
+                    dataType = parseDataType(args, i);
+                    i++; // skip next arg
+                    break;
+                case "-sortingType":
+                    sortingType = parseSortingType(args, i);
+                    i++; // skip next arg
+                    break;
+                case "-inputFile":
+                    readFromFile = true;
+                    readPath = parseFilePath(args, i);
+                    i++; // skip next arg
+                    break;
+                case "-outputFile":
+                    writeToFile = true;
+                    writePath = parseFilePath(args, i);
+                    i++; // skip next arg
+                    break;
+                default:
+                    printUsage();
+                    throw new IllegalArgumentException("Unknown argument: " + args[i]);
             }
         }
+    }
+
+    private DataType parseDataType(String[] args, int index) {
+        if (index + 1 >= args.length) {
+            throw new IllegalArgumentException("Missing value for -dataType");
+        }
+        try {
+            return DataType.fromString(args[index + 1]);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid data type: " + args[index + 1], e);
+        }
+    }
+
+    private SortingType parseSortingType(String[] args, int index) {
+        if (index + 1 >= args.length) {
+            throw new IllegalArgumentException("Missing value for -sortingType");
+        }
+        try {
+            return SortingType.fromString(args[index + 1]);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid sorting type: " + args[index + 1], e);
+        }
+    }
+
+    private String parseFilePath(String[] args, int index) {
+        if (index + 1 >= args.length) {
+            throw new IllegalArgumentException("Missing value for file path");
+        }
+        return args[index + 1];
     }
 
     public boolean isReadFromFile() {
@@ -71,6 +93,10 @@ public class ArgumentParser {
         System.out.println("Usage:");
         System.out.println("  -dataType <type>    Specify the data type (e.g., LONG, LINE, WORD)");
         System.out.println("  -sortingType <type> Specify the sorting type (e.g., NATURAL, BY_COUNT, etc.)");
+        System.out.println("  -inputFile <path>   Specify the input file path");
+        System.out.println("  -outputFile <path>  Specify the output file path");
+        System.out.println("Examples:");
+        System.out.println("  java Main -dataType LONG -sortingType NATURAL -inputFile data.txt -outputFile result.txt");
     }
 
     public String getWritePath() {
